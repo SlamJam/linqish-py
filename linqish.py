f27e2c32088a4331f07b85ea7b39f2f8e948bb64
Henri Wiechers <hwiechers@gmail.com>
2011-01-17 15:56:51 +0200

    changed _ReverseKey to use rich comparison operators

--- linqish.py
+++ linqish.py
@@ -11,13 +11,23 @@ class _ReverseKey(object):
     def __init__(self, key):
         self._key = key
 
-    def __cmp__(self, other):
-        if self._key < other._key:
-            return 1
-        elif self._key > other._key:
-            return -1
-        else:
-            return 0
+    def __eq__(self, other):
+        return self._key == other._key
+
+    def __lt__(self, other):
+        return self._key > other._key
+
+    def __gt__(self, other):
+        return self._key < other._key
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __le__(self, other):
+        return self.__lt__(other) or self.__eq__(other)
+
+    def __ge__(self, other):
+        return self.__gt__(other) or self.__eq__(other)
 
 class _Grouping(object):
     def __init__(self, key, elements):
